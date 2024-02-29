5a8e54e0cf91d30a340cfdca89b083094f4bed80
ParadosBoy <1057458756@outlook.com>
2024-02-29 10:41:54 +0800

    提交信息

---
+++ 提交内容/测试脚本/features2d-perf.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+input=opencv_perf_features2d_list_names.txt
+output=opencv_perf_features2d_results.txt
+tmp_dir=testdetail
+mkdir -p $tmp_dir
+rm -f $tmp_dir/*
+date >>  $output
+export LD_LIBRARY_PATH='/root/lib/:$LD_LIBRARY_PATH'
+while read -r line
+do
+                #把测试名称保存到文件
+        echo $line >> $output
+                #把每一项的测试详情保存到文件
+        ./opencv_perf_features2d  --gtest_filter="*$line*" 2>&1 | tee -a $tmp_dir/"$line"_detail.txt
+                #把测试是否通过的结果保存到文件
+        echo $? >> $output
+done <"$input"
+echo "Test end @" >> $output
+date >>  $output
